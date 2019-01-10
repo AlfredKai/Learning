824ac1358870ad06ed3546e8d5dfc83b568b6a56
Alfred_Chang <Alfred_Chang@bridgewell.com>
2019-01-10 18:14:57 +0800

    add ref type,  value type and ref/out

---
+++ PassByReference/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace PassByReference
+{
+    public struct ValueType
+    {
+        public int num;
+    }
+
+    public class ReferenceType
+    {
+        public int num;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Console.WriteLine($"int before: {a}");
+            ChangeNum(ref a);
+            Console.WriteLine($"int after: {a}");
+            Console.WriteLine();
+
+            ValueType c = new ValueType();
+            Console.WriteLine($"Value type with 'ref' keyword before: {c.num}");
+            ChangeNum(ref c);
+            Console.WriteLine($"Value type with 'ref' keyword after: {c.num}");
+            Console.WriteLine();
+
+            ReferenceType d = new ReferenceType();
+            Console.WriteLine($"Reference type 'ref' keyword before: {d.num}");
+            ChangeNum(ref d);
+            Console.WriteLine($"Reference type 'ref' keyword after: {d.num}");
+            Console.WriteLine();
+        }
+        public static void ChangeNum(ref int p)
+        {
+            p = 1;
+        }
+
+        public static void ChangeNum(int[] a)
+        {
+            a[1] = 9;
+        }
+
+        public static void ChangeNum(ref int[] a)
+        {
+            a[1] = 9;
+        }
+
+        public static void ChangeNum(ValueType p)
+        {
+            p.num = 2;
+        }
+
+        public static void ChangeNum(ref ValueType p)
+        {
+            p.num = 2;
+        }
+
+        public static void ChangeNum(ReferenceType p)
+        {
+            var g = new ReferenceType();
+            g.num = 2;
+            p = g;
+        }
+
+        public static void ChangeNum(ref ReferenceType p)
+        {
+            var g = new ReferenceType();
+            g.num = 2;
+            p = g;
+        }
+    }
+}
---
+++ RefAndValueType/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace RefAndValueType
+{
+    public struct ValueType
+    {
+        public int num;
+    }
+
+    public class ReferenceType
+    {
+        public int num;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Console.WriteLine($"int before: {a}");
+            ChangeNum(a);
+            Console.WriteLine($"int after: {a}");
+            Console.WriteLine();
+
+            ValueType b = new ValueType();
+            Console.WriteLine($"Value type before: {b.num}");
+            ChangeNum(b);
+            Console.WriteLine($"Value type after: {b.num}");
+            Console.WriteLine();
+
+            ReferenceType c = new ReferenceType();
+            Console.WriteLine($"Reference type before: {c.num}");
+            ChangeNum(c);
+            Console.WriteLine($"Reference type after: {c.num}");
+            Console.WriteLine();
+
+            int[] intArray = new int[] { 1, 2, 3 };
+            Console.WriteLine($"int array before: {intArray[1]}");
+            ChangeNum(intArray);
+            Console.WriteLine($"int array after: {intArray[1]}");
+
+            Console.Read();
+        }
+
+        public static void ChangeNum(int p)
+        {
+            p = 1;
+        }
+
+        public static void ChangeNum(int[] a)
+        {
+            a[1] = 9;
+        }
+        
+
+        public static void ChangeNum(ValueType p)
+        {
+            p.num = 2;
+        }
+
+        public static void ChangeNum(ReferenceType p)
+        {
+            p.num = 2;
+        }
+    }
+}
